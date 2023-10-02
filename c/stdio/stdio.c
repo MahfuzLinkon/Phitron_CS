1000f6dfd8661646067fb5ea4212d0cd455f0026
mahfuzlinkon <mahfuzur076@gmail.com>
2023-10-02 20:31:02 +0600

    C variable

---
+++ c/stdio/stdio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // Print 
+    // printf("HELLO\n");
+    // printf("100%%");
+
+    // Input
+    int a, b;
+    scanf("%d%% %d%%", &a, &b);
+    printf("%d%% %d%%", a, b);
+
+    return 0;
+}
---
+++ c/variables/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a = 10000000000;
+    printf("%lld \n", a);
+
+    float f = 200.111252;
+    printf("%f \n", f);
+
+    double d = 2.1112;
+    printf("%-8.2lf ", d);
+
+    return 0;
+}
